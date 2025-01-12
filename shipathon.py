7fcb33a96bc7c73655ff3115c48cffdd4d44c257
HarshVerma298 <25harsh.verma07@gmail.com>
2025-01-12 15:31:47 +0530

    shipathon.py

--- shipathon.py
+++ shipathon.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.header("Welcome to Your Ultimate Campus Companion: IIT Delhi AI Assistant")
 st.text("Embarking on your journey at IIT Delhi can be as thrilling as it is challenging. That’s where our AI Assistant steps in! Designed to simplify, guide, and enrich every aspect of your campus life, this intelligent tool is your go-to companion for everything IIT Delhi has to offer.")
 
-name = st.text_input("Enter your query")
+name = st.text_area("Enter your query")
 if st.button("Enter"):
   if name:
     st.write(f"Getting to your query, {name}!")
