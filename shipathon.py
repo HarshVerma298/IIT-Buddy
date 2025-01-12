9730ca7c09745a308b657acc9f8ee38b337f1437
HarshVerma298 <25harsh.verma07@gmail.com>
2025-01-12 14:59:57 +0530

    shipathon.py

--- shipathon.py
+++ shipathon.py
@@ -6,3 +6,5 @@ st.header("Welcome to Your Ultimate Campus Companion: IIT Delhi AI Assistant")
 st.text("Embarking on your journey at IIT Delhi can be as thrilling as it is challenging. That’s where our AI Assistant steps in! Designed to simplify, guide, and enrich every aspect of your campus life, this intelligent tool is your go-to companion for everything IIT Delhi has to offer.")
 
 name = st.text_input("Enter your query")
+if name:
+  st.write(f"Getting to your query, {name}!")
