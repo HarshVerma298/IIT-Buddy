df0e0d91ac5d453e6b3e24979e6b38eb972d12d1
HarshVerma298 <25harsh.verma07@gmail.com>
2025-01-12 14:54:46 +0530

    Add files via upload

---
+++ shipathon.py
@@ -0,0 +1,8 @@
+
+import streamlit as st
+
+
+st.header("Welcome to Your Ultimate Campus Companion: IIT Delhi AI Assistant")
+st.text("Embarking on your journey at IIT Delhi can be as thrilling as it is challenging. That’s where our AI Assistant steps in! Designed to simplify, guide, and enrich every aspect of your campus life, this intelligent tool is your go-to companion for everything IIT Delhi has to offer.")
+
+name = st.text_input("Enter your query")
